f622bfb2462ee6999a3b50283bacaaf16ef05a8d
Yoana Encheva <je.encheva@gmail.com>
2021-11-27 13:48:44 +0200

    modify links to prevent app reloading on click

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,4 +1,5 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,14 +9,26 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/classes">Classes</Nav.Link>
-            <Nav.Link href="/coaches">Coaches</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/classes">
+              Classes
+            </Nav.Link>
+            <Nav.Link as={Link} to="/coaches">
+              Coaches
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contacts">
+              Contacts
+            </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/blog">Blog</Nav.Link>
+            <Nav.Link as={Link} to="/blog">
+              Blog
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
